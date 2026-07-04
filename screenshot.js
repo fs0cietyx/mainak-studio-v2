215807c1e44affcd02667a3ffadbcd083e0795ca
Mainak Biswas <mainakbiswas22@gmail.com>
2026-07-04 21:36:56 +0530

    chore: major repository cleanup, move scripts, add gitignore rules

--- screenshot.js
+++
@@ -1,9 +0,0 @@
-const { chromium } = require('playwright');
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage({ viewport: { width: 1280, height: 720 } });
-  await page.goto('http://localhost:5173');
-  await page.waitForTimeout(2000);
-  await page.screenshot({ path: 'screenshot.png' });
-  await browser.close();
-})();
